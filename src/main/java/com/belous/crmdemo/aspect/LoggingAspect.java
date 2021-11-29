9b6d8289fc80535517ca79c35ad43b9248b060c9
BelousAnton <anton.belous@customertimes.com>
2021-11-29 21:02:34 +0300

    Setup pointcut declarations

--- src/main/java/com/belous/crmdemo/aspect/LoggingAspect.java
+++ src/main/java/com/belous/crmdemo/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.belous.crmdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -9,5 +10,22 @@ import java.util.logging.Logger;
 @Component
 public class LoggingAspect {
 
+    // setup logger
     private final Logger myLogger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declarations
+
+    @Pointcut("execution(* com.belous.crmdemo.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.belous.crmdemo.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("execution(* com.belous.crmdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {}
+
+
 }
